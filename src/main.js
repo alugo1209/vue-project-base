6f3b012cf89ec7fe97146fc1fe2ba6cc21f38ff7
aelm1209 <angel.ernesto@gmail.com>
2024-01-24 10:17:36 -0500

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { createI18n } from 'vue-i18n'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(VueAxios, axios)
+// app.use(BootstrapVue)
+// app.use(IconsPlugin)
+app.use(createI18n)
+
+app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
